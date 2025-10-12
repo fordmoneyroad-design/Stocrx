81d3f4e0512848d81d35a3f53eb02a87fd94c15a
fordmoneyroad-design <fordmoneyroad@gmail.com>
2025-10-12 02:59:39 -0400

    Updated supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
-const supabaseUrl = "https://tnrxloldwmrlwslftepl.supabase.co"
-const supabaseAnonKey = "YOUR_ANON_KEY"
+const supabaseUrl = "https://bqdkkorreefnoyxftkyw.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxZGtrb3JyZWVmbm95eGZ0a3l3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxODk3MjQsImV4cCI6MjA3NTc2NTcyNH0.Kv1-tm4RCQdL1wBaERB2Oaxjzi1vEnn9HpwErwfeZEI"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
